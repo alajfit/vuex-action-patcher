6026a4092a29d5d7943af4f0705fa9bca400c4fe
Alan Fitzpatrick <alajfit@gmail.com>
2019-02-21 19:50:00 +0000

    Starting Vuex Action Patcher

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import isPromise from './utils/isPromise'
+import patchAction from './utils/patchAction'
+import patchModule from './utils/patchModule'
+
+/**
+ * @name vuexActionPatcher
+ * @desc Patch the main state and all registered submodules
+ * @param tools An { object | array } of tools to be added to the Vuex context
+ */
+export default function vuexActionPatcher (tools) {
+    return store => {
+        // Patch the original store
+
+        // Augment the registerModule to add tools
+        
+    }
+}
