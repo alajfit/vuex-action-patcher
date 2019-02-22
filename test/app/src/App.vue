8d18528ead6642003f4fe95b0134ec5f120258cb
Alan Fitzpatrick <alajfit@gmail.com>
2019-02-22 17:08:12 +0000

    Added demo app

---
+++ test/app/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Vuex Action Patcher Demo</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<style lang="scss">
+
+</style>
