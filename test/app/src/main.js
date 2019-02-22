084a886bc649aa0a83701784e2c9f4001d871e1d
Alan Fitzpatrick <alajfit@gmail.com>
2019-02-22 17:33:59 +0000

    Started basic setup of the app

--- test/app/src/main.js
+++ test/app/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import App from './App.vue'
+import store from './store'
 
 Vue.use(Vuex)
 
 new Vue({
   el: '#app',
+  store,
   render: h => h(App)
 })
