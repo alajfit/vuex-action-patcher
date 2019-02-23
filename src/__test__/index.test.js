6f8da8e1037b342bf6ca19aa8ca3e6961a04eee4
Alan Fitzpatrick <alajfit@gmail.com>
2019-02-23 22:44:07 +0000

    Bringing up the test coverage

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,29 +1,73 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import Vue from 'vue'
 import Vuex from 'vuex'
+import moment from 'moment'
 import vuexActionPatcher from '../index'
+
+process.env.HARU_DEV = true
+process.env.VUE_ENV = 'server'
+
+const MockComponent = { template: '<div></div>' }
+const localVue = createLocalVue()
 
 describe('Vuex Action Patcher', () => {
 
-    it('Expose a function to create a plugin', () => {
-        expect(typeof vuexActionPatcher).toBe('function')
+    describe('Setup Tests', () => {
+        it('Expose a function to create a plugin', () => {
+            expect(typeof vuexActionPatcher).toBe('function')
+        })
+    
+        it('Should create a function when the constructor is called', () => {
+            const vuexActionPatch = vuexActionPatcher(Vuex, { moment })
+            expect(typeof vuexActionPatch).toBe('function')
+        })
+    
+        it('It should fail gracefully when Vuex is not passed', () => {
+            const vuexActionPatch = vuexActionPatcher({ moment })
+            expect(typeof vuexActionPatch).toBe('function')
+        })
+    
+        it('It should fail gracefully when no tools are passed', () => {
+            const vuexActionPatch = vuexActionPatcher(Vuex)
+            expect(typeof vuexActionPatch).toBe('function')
+        })
+    
+        it('Created object should be passable to the Vuex plugins', () => {
+            Vue.use(Vuex)
+            const vuexActionPatch = vuexActionPatcher(Vuex, { moment })
+            const store = new Vuex.Store({
+                state: { name: 'test' },
+                plugins: [vuexActionPatch]
+            })
+            expect(typeof store).toBe('object')
+        })
     })
 
-    it('Should create an object when the constructor is called', () => {
-        expect(true).toBe(true)
-    })
+    describe('Mounted Test', () => {
+        let comp
+        localVue.use(Vuex)
 
-    it('It should fail gracefully when Vuex is not passed', () => {
-        expect(true).toBe(true)
-    })
+        const vuexActionPatch = vuexActionPatcher(Vuex, { moment })
+        const store = new Vuex.Store({
+            state: { name: 'test', time: null },
+            actions: { 
+                time({ moment }) {
+                    console.log(moment().startOf('day').fromNow())
+                }
+            },
+            plugins: [vuexActionPatch]
+        })
 
-    it('It should fail gracefully when no tools are passed', () => {
-        expect(true).toBe(true)
-    })
+        beforeEach(() => {
+            comp = shallowMount(MockComponent, {
+                localVue,
+                store
+            })
+        })
 
-    it('Created object should be passable to the Vuex plugins', () => {
-        expect(true).toBe(true)
-    })
-
-    it('When a properly constructed object is passed as a plugin it should expose the tools', () => {
-        expect(true).toBe(true)
+        it('When a properly constructed object is passed as a plugin it should expose the tools', () => {
+            comp.vm['$store']._actions.time[0]()
+            expect(global.console.log).toBeCalled()
+        })
     })
 })
--- src/index.js
+++ src/index.js
@@ -5,28 +5,36 @@ import utils from './utils'
  * @desc Patch the main state and all registered submodules
  * @param tools An { object } of tools to be added to the Vuex context
  */
-export default function vuexActionPatcher (Vuex, tools = {}) {
-    return store => {
-        // Taking a copy of the original registerModule function
-        const copyRegisterModule = Vuex.Store.prototype.registerModule
+export default function vuexActionPatcher (Vuex, tools) {
+    return typeof Vuex === 'object' && Vuex.Store && typeof tools === 'object'
+        ? store => {
+            // Taking a copy of the original registerModule function
+            const copyRegisterModule = Vuex.Store.prototype.registerModule
 
-        // Patch the Core Module
-        utils.patchModule({
-            store,
-            path: [],
-            module: store._modules.root,
-            tools
-        })
-
-        // Patch Modules registered later
-        Vuex.Store.prototype.registerModule = function registerModule (path, rawModule) {
-            copyRegisterModule.call(this, path, rawModule)
+            // Patch the Core Module
             utils.patchModule({
-                store: this,
-                path: [].concat(path),
-                module: this._modules.get([path]),
+                store,
+                path: [],
+                module: store._modules.root,
                 tools
             })
+
+            // Patch Modules registered later
+            Vuex.Store.prototype.registerModule = function registerModule (path, rawModule) {
+                copyRegisterModule.call(this, path, rawModule)
+                utils.patchModule({
+                    store: this,
+                    path: [].concat(path),
+                    module: this._modules.get([path]),
+                    tools
+                })
+            }
         }
-    }
+        : store => {
+            throw new Error(
+                typeof Vuex !== 'object' || !Vuex.Store
+                    ? 'Vuex must be passed as the first argument'
+                    : 'Utils must be passed in an object as the second argument'
+            )
+        }
 }
