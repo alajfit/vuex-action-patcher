c47d00bf23fb4b297261b2a582ad5859ae63dca4
Alan Fitzpatrick <alajfit@gmail.com>
2019-02-23 10:50:19 +0000

    Updating ignore

--- test/app/src/store/index.js
+++ test/app/src/store/index.js
@@ -1,10 +1,15 @@
 import Vuex from 'vuex'
 import { name } from '../../../../package.json'
+import vuexActionPatcher from 'vuex-action-patcher'
+import moment from 'moment'
+
+const tools = vuexActionPatcher({moment})
 
 export default function () {
     return new Vuex.Store({
         state: {
             name
-        }
+        },
+        plugins: [tools]
     })
 }
